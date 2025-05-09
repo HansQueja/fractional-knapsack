80bf37bccb3992bd6cae83eacca0187187caa9b3
Hans Queja <hansqueja8@gmail.com>
2025-05-09 22:33:54 +0800

    Add stock.py for stock class

---
+++ helpers/stock.py
@@ -0,0 +1,11 @@
+
+class Stock:
+    def __init__(self, name, price, profit):
+        self.name = name
+        self.price = price
+        self.profit = profit
+        self.ROI = 0
+
+    def __str__(self):
+        return f"Name: {self.name}\nPrice: {self.price}\nReturn Profit: {self.profit}"
+    
