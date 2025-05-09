f6c77ccd1eabddcd09d2bf66cb92967685fab133
Hans Queja <hansqueja8@gmail.com>
2025-05-09 22:32:04 +0800

    Add main program file

---
+++ main.py
@@ -0,0 +1,20 @@
+
+from helpers.menu import menu
+from helpers.knapsack import fractional_knapsack
+from helpers.stock import Stock
+
+def stock_application():
+    
+    # Ask user if they want to type the values or read an existing JSON file
+    # Store data that the algorithm will process
+    stock_list, budget = menu()
+
+    # Call the algorithm with the data provided 
+    # Get a return value of the choices made, and the total profit
+    fractional_knapsack(stock_list, budget)
+
+    # Ask the user if they want to run the application again
+
+
+if __name__ == "__main__":
+    stock_application()
