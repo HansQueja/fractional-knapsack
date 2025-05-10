4810f41b6aad7dfdcabdfcd4a8e265688eba8327
Hans Queja <hansqueja8@gmail.com>
2025-05-10 13:59:35 +0800

    Refactor knapsack algorithm and add comments

--- helpers/knapsack.py
+++ helpers/knapsack.py
@@ -1,25 +1,28 @@
 
-from helpers.stock import Stock
-
 def fractional_knapsack(stock_list, remaining_budget):
     
     # Compute for the Profit per Cost Ratio (ROI) for each stock
     for stock in stock_list:
         stock.ROI = stock.profit / stock.price
 
-    # Sort stocks in descending order based on their ROI
+    # Sort stocks in descending order based on their ROI which takes O(nlogn)
     stock_list.sort(key=lambda stock: stock.ROI, reverse=True)
 
     count = 0
     total_profit = 0
+    
+    print("==============================================================================\n")
 
     # Choose the stock with the largest ROI
     while count < len(stock_list):
+
+        print(f"\tInvested Stock #{count + 1}")
 
         # If stock price is within the remaining budget, invest in the stock
         if stock_list[count].price <= remaining_budget:
             remaining_budget -= stock_list[count].price
             total_profit += stock_list[count].profit
+
             print(f"\tName: {stock_list[count].name}\n\tPrice: {stock_list[count].price}\n\tProfit: {stock_list[count].profit}\n")
             count += 1
 
@@ -27,8 +30,13 @@ def fractional_knapsack(stock_list, remaining_budget):
         else:
             fraction_cost = remaining_budget / stock_list[count].price
             total_profit += round(fraction_cost * stock_list[count].profit, 3)
-            print(f"\tName: {stock_list[count].name}\n\tPrice: {fraction_cost}\n\tProfit: {round(fraction_cost * stock_list[count].profit, 3)}\n")
+
+            print("\t(Fractional Stock)")
+            print(f"\tName: {stock_list[count].name}\n\tPrice: {fraction_cost * stock_list[count].price}\n\tProfit: {round(fraction_cost * stock_list[count].profit, 3)}\n")
+            count += 1
             break
     
     # Show the total profit we've accumulated
-    print(f"\n\tTotal profit: Php. {total_profit}")
+    print(f"\tTotal profit: Php. {total_profit}")
+    print(f"\tTotal number of stocks: {count}")
+    print("\n==============================================================================\n")
